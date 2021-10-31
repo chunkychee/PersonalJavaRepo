472d06ac1b71f372d84287b0ecaceafc54036eee
chunkychee <biontay55@gmail.com>
2021-10-31 10:02:47 +0800

    files

---
+++ src/cardType.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class cardType {
+
+    public static void main(String[] args) {
+        int i;
+        int numOfTypeCards;
+        int counter = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("input the number of types of cards");
+        numOfTypeCards = input.nextInt();
+        int[] cardArray = new int[numOfTypeCards];
+        System.out.println("input the pre-determine amt of cards");
+
+        for (i = 0; i <numOfTypeCards; i++) {
+            int inputcard = input.nextInt();
+            if (inputcard % 2 == 0) {
+                cardArray[i] = inputcard;
+            } else {
+                inputcard+=1;
+                cardArray[i] = inputcard;
+                counter+=1;
+            }
+            System.out.println(Arrays.toString(cardArray));
+            System.out.println(counter);
+        }
+    }
+}
